94d65424e61fef3f1e357ddd530733ff7c0ffdf6
Bindushree N <bindun22@in.ibm.com>
2019-03-20 16:54:23 +0530

    adding reflctions class

---
+++ labs/CoreJava/reflections/ReflectionEx.java
@@ -0,0 +1,26 @@
+package com.examples.core.java;
+
+public class ReflectionEx {
+
+	public ReflectionEx(){
+		
+	}
+	
+	public Integer add(Integer a ,Integer b)
+	{
+		return a+b;
+	}
+	
+	public int sub(Integer a ,Integer b)
+	{
+		return a-b;
+	}
+	public int mul(Integer a ,Integer b)
+	{
+		return a*b;
+	}public int div(Integer a ,Integer b)
+	{
+		return a/b;
+	}
+	
+}
---
+++ labs/CoreJava/reflections/ReflectionMain.java
@@ -0,0 +1,34 @@
+package com.examples.core.java;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class ReflectionMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+try {
+	Class<?> c= ReflectionEx.class;
+	//Class<?> c1= Class.forName("com.examples.core.java.ReflectionEx");
+	
+	System.out.println(c);
+	Constructor<?> constr =c.getConstructor();
+	System.out.println(constr);
+	
+	Object obj =constr.newInstance();
+	
+	Method add= c.getMethod("add", Integer.class,Integer.class);
+	Object x= add.invoke(obj,5, 6);
+	System.out.println(x);
+	
+}catch (Exception e) {
+	// TODO: handle exception
+	
+	e.printStackTrace();
+}
+
+		
+		
+	}
+
+}
